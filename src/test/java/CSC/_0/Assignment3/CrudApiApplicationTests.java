9aee69524d6aea058ddddc4237ddf8b8bc4b8e04
Mariono Deleva <mdeleva888@gmail.com>
2025-10-09 16:31:24 -0400

    spring boot file upload

---
+++ src/test/java/CSC/_0/Assignment3/CrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package CSC._0.Assignment3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
