419436c32e43be3f8a855071c5e1410c746703a8
Mariono Deleva <mdeleva888@gmail.com>
2025-10-10 22:56:19 -0400

    final

--- src/main/java/CSC/_0/Assignment3/Cat/CatController.java
+++ src/main/java/CSC/_0/Assignment3/Cat/CatController.java
@@ -2,6 +2,9 @@ package CSC._0.Assignment3.Cat;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
